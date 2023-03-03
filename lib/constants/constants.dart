fb9ce311b6cc5220bb54acf7fdaf3b555059cdf1
MohamedAbdelrehem <mohamedabdelrehem32@gmail.com>
2023-03-03 15:14:49 +0200

    add constants to control theme

---
+++ lib/constants/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color mainTheme = Colors.lightGreen;
+  static const Color secoundryTheme = Color(0xFF5D9EDF);
+  static const Color backcolor = Colors.tealAccent;
+}
