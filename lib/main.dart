088ba155f5015a6e0b200ec2279554ad9a6e1b51
MohamedAbdelrehem <mohamedabdelrehem32@gmail.com>
2023-03-05 22:02:12 +0200

    add hiveDatabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/models/note_data.dart';
 import 'package:notes_app/pages/home_page.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  //initialize hive
+  await Hive.initFlutter();
+  //open hive box
+  await Hive.openBox('note_database');
   runApp(const MyApp());
 }
 
