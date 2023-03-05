df415c06207a2697da926a725a932dbf0b3b9d68
MohamedAbdelrehem <mohamedabdelrehem32@gmail.com>
2023-03-05 23:43:17 +0200

    set intial notes

--- lib/models/note_data.dart
+++ lib/models/note_data.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/data/hive_database.dart';
 import 'package:notes_app/models/note.dart';
 
 class NoteData extends ChangeNotifier {
+//hive database
+  final db = HiveDatabase();
 //overall list note
   List<Note> allNotes = [
     //defult the first note
     Note(id: 0, text: 'first note')
   ];
+//initialize the list
+  void initializeNotes() {
+    allNotes = db.loadNotes();
+  }
+
 //get the note
   List<Note> getAllNotes() {
     return allNotes;
